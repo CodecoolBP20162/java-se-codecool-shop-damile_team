0601dea547d05156931dc3d12f18a91d5cbf1179
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-04-27 15:30:24 +0200

    text changed at iphone 9 to 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -84,7 +84,7 @@ public class Main {
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 
-        productDataStore.add(new Product("Iphone 9", 649f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", mobile, apple));
+        productDataStore.add(new Product("Iphone 7", 649f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", mobile, apple));
         productDataStore.add(new Product("Samsung Galaxy Note 7", 969, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", mobile, samsung));
         productDataStore.add(new Product("HTC Desire", 90, "USD", "Our HTC Desire is a great value for media consumption.", mobile, others));
 
