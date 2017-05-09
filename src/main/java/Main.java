c75b87586e458da0929c7fd0a2bd78f58d782e3b
David Szilard <szilard.david91@gmail.com>
2017-05-09 19:36:59 +0200

    deleted supplier and category instances

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
 import com.codecool.shop.controller.ProductController;
-import com.codecool.shop.dao.DaoWithJDBC;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.DaoMemWithJDBC;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.Product;
+import com.codecool.shop.dao.*;
+import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import spark.Request;
@@ -20,8 +13,12 @@ import static spark.debug.DebugScreen.enableDebugScreen;
 public class Main {
 
     public static void main(String[] args) {
-        DaoWithJDBC daoWithJDBC = new DaoMemWithJDBC();
-        System.out.println(daoWithJDBC.listAllProducts());
+        ProductDaoWithJDBC productDaoWithJDBC = new ProductDaoMemWithJDBC();
+        System.out.println(productDaoWithJDBC.listAllProducts());
+        ProductCategoryDaoWithJDBC productCategoryDaoWithJDBC = new ProductCategoryDaoMemWithJDBC();
+        System.out.println(productCategoryDaoWithJDBC.getAllCategories());
+        SupplierDaoWithJDBC supplierDaoWithJDBC = new SupplierDaoMemWithJDBC();
+        System.out.println(supplierDaoWithJDBC.getAllSupplier());
         // default server settings
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
         staticFileLocation("/public");
@@ -53,29 +50,6 @@ public class Main {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-
-        //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
-        Supplier apple = new Supplier("Apple", "Mobile phones");
-        supplierDataStore.add(apple);
-        Supplier samsung = new Supplier("Samsung", "Computers and mobile phones");
-        supplierDataStore.add(samsung);
-        Supplier others = new Supplier("Others", "Digital content and services");
-        supplierDataStore.add(others);
-
-        //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
-
-        ProductCategory mobile = new ProductCategory("Mobile", "Hardware", "A mobile phone is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area.");
-        productCategoryDataStore.add(mobile);
-
-        ProductCategory tv = new ProductCategory("TV", "Hardware", "A flat panel LCD TV set that uses LEDs (light emitting diodes) for its backlight source rather than the earlier cold cathode fluorescent lamps.");
-        productCategoryDataStore.add(tv);
-
 
     }
 
