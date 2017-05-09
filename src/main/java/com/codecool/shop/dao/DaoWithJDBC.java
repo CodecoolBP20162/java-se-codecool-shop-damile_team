785bc17222aa621e6a903633f826dc791c4e4955
David Szilard <szilard.david91@gmail.com>
2017-05-09 11:25:43 +0200

    created Daos with JDBC

--- src/main/java/com/codecool/shop/dao/DaoWithJDBC.java
+++ src/main/java/com/codecool/shop/dao/DaoWithJDBC.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao;
 import java.sql.*;
+import java.util.List;
 
 public interface DaoWithJDBC {
-
+    public List<Object> listAllProducts();
 }
--- src/main/java/com/codecool/shop/dao/implementation/DaoMemWithJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/DaoMemWithJDBC.java
@@ -2,13 +2,24 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.DaoWithJDBC;
 
+import java.awt.*;
 import java.sql.*;
+import java.util.*;
+import java.util.List;
 
 public class DaoMemWithJDBC implements DaoWithJDBC {
 
     private static final String DATABASE = "jdbc:postgresql://localhost:5432/todolist";
     private static final String DB_USER = "szilarddavid";
     private static final String DB_PASSWORD = "szilarddavid";
+
+    @Override
+    public List<Object> listAllProducts() {
+        String query = "SELECT * FROM products;";
+
+        List<Object> resultList = new ArrayList<>();
+        return resultList;
+    }
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
